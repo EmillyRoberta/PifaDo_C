5266567feb231c3b46f11cb7d357f36d3d0a89af
EmillyRoberta <50766387+EmillyRoberta@users.noreply.github.com>
2021-06-25 00:35:54 -0300

    Update Aplicacao.c

--- Aplicacao.c
+++ Aplicacao.c
@@ -105,30 +105,32 @@ int main()
     e4J2=(Deque*) malloc(sizeof(Deque));
     inicDeque(e4J2);
 
-
-
+    printf("\t****************************\n\t*          PiFaDo          *\n\t****************************\n\t*    Welcome to the game.  *\n\t*        Boa sorte!        *\n\t****************************\n\t*       Emilly - 5978     *\n\t****************************\n");
+    printf("\t-> Carregando...");
+    Sleep(5000);
+    system("cls || clear");
     do
     {
-        /*verifica se é a PRIMEIRA rodada*/
+        /*verifica se ï¿½ a PRIMEIRA rodada*/
         if(vez == 0)
         {
             if(vez %2 == 0)  /*jogador 1*/
             {
-                printf("****** JOGADOR 1***********\n");
+                printf("\t********* JOGADOR 1 ********* \n\t-> Score: %d\n", pontoJ1);
                 carta = compraMonte(monte1); /*compra a carta do monte*/
-                printf("\n%d %s\n\n", carta.num, carta.nome);
-                pontoJ1 = pontoJ1 + menu(monte1, e1J1, e2J1, e3J1, e4J1, &extra1, carta); /*chama a função menu e incrementa no ponto se o espaço tiver 3 cartas*/
+                printf("\t-> Carta da rodada: %d %s\n\n", carta.num, carta.nome);
+                pontoJ1 = pontoJ1 + menu(monte1, e1J1, e2J1, e3J1, e4J1, &extra1, carta); /*chama a funï¿½ï¿½o menu e incrementa no ponto se o espaï¿½o tiver 3 cartas*/
                 vez++;
             }
 
             if(vez %2 != 0)  /*JOGADOR 2*/
             {
-                system("cls");
-                printf("****** JOGADOR 2***********\n");
+                system("cls || clear");
+                printf("\t********* JOGADOR 2 ********* \n\t-> Score: %d\n", pontoJ2);
                 carta = compraMonte(monte2); /*compra a carta do monte*/
-                printf("\n%d %s\n\n", carta.num, carta.nome);
+                printf("\t-> Carta da rodada: %d %s\n\n", carta.num, carta.nome);
 
-                pontoJ2 = pontoJ2 + menu(monte2, e1J2, e2J2, e3J2, e4J2, &extra2, carta); /*chama a função menu e incrementa no ponto se o espaço tiver 3 cartas*/
+                pontoJ2 = pontoJ2 + menu(monte2, e1J2, e2J2, e3J2, e4J2, &extra2, carta); /*chama a funï¿½ï¿½o menu e incrementa no ponto se o espaï¿½o tiver 3 cartas*/
                 vez++;
 
             }
@@ -143,19 +145,19 @@ int main()
             {
                 int compra;
 
-                printf("****** JOGADOR 1***********\n");
+                printf("\t********* JOGADOR 1 ********* \n\t-> Score: %d\n\n", pontoJ1);
                 mostrarEspacos(e1J1, e2J1, e3J1, e4J1); /*caso o usuario queira ver as cartas dos espacos*/
                 printf("Deseja comprar de onde?\n");
                 printf("1. Monte\n");
                 printf("2. Extra\n");
-                printf("insira: ");
+                printf("Insira: ");
                 scanf("%d", &compra);
 
                 if(compra == 1)
                 {
                     carta = compraMonte(monte1); /*compra a carta do monte*/
-                    printf("\n%d %s\n\n", carta.num, carta.nome);
-                    pontoJ1 = pontoJ1 + menu(monte1, e1J1, e2J1, e3J1, e4J1, &extra1, carta);/*chama a função menu e incrementa no ponto se o espaço tiver 3 cartas*/
+                    printf("\n\t-> Carta da rodada: %d %s\n\n", carta.num, carta.nome);
+                    pontoJ1 = pontoJ1 + menu(monte1, e1J1, e2J1, e3J1, e4J1, &extra1, carta);/*chama a funï¿½ï¿½o menu e incrementa no ponto se o espaï¿½o tiver 3 cartas*/
                     vez ++;
                 }
 
@@ -165,15 +167,15 @@ int main()
                     /*verifica se o extra esta vazio*/
                     if (listaVaziaPilha(extra1)==1)
                     {
-                        printf("espaco extra vazio\n");
+                        printf("Espaco extra vazio\n");
                         Sleep(1000);
                     }
                     else
                     {
                         carta = compraExtra(&extra1);
-                        printf("\n%d %s\n\n", carta.num, carta.nome);
+                        printf("\n\t-> Carta da rodada: %d %s\n\n", carta.num, carta.nome);
 
-                        pontoJ1 = pontoJ1 + menu(monte1, e1J1, e2J1, e3J1, e4J1, &extra1, carta);/*chama a função menu e incrementa no ponto se o espaço tiver 3 cartas*/
+                        pontoJ1 = pontoJ1 + menu(monte1, e1J1, e2J1, e3J1, e4J1, &extra1, carta);/*chama a funï¿½ï¿½o menu e incrementa no ponto se o espaï¿½o tiver 3 cartas*/
 
                         vez ++;
                     }
@@ -186,20 +188,20 @@ int main()
                 {
                     system("cls || clear");
                     int compra;
-                    printf("****** JOGADOR 2***********\n");
+                    printf("\t********* JOGADOR 2 ********* \n\t-> Score: %d\n\n", pontoJ2);
                     mostrarEspacos(e1J2, e2J2, e3J2, e4J2);
                     printf("Deseja comprar de onde?\n");
                     printf("1. Monte\n");
                     printf("2. Extra\n");
 
-                    printf("insira: ");
+                    printf("Insira: ");
                     scanf("%d", &compra);
 
                     if(compra == 1)
                     {
                         carta = compraMonte(monte2); /*compra a carta do monte*/
-                        printf("\n%d %s\n\n", carta.num, carta.nome);
-                        pontoJ2 = pontoJ2 + menu(monte2, e1J2, e2J2, e3J2, e4J2, &extra2, carta);/*chama a função menu e incrementa no ponto se o espaço tiver 3 cartas*/
+                        printf("\n\t-> Carta da rodada: %d %s\n\n", carta.num, carta.nome);
+                        pontoJ2 = pontoJ2 + menu(monte2, e1J2, e2J2, e3J2, e4J2, &extra2, carta);/*chama a funï¿½ï¿½o menu e incrementa no ponto se o espaï¿½o tiver 3 cartas*/
                         vez++;
                     }
                     else
@@ -214,29 +216,29 @@ int main()
                         else
                         {
                             carta = compraExtra(&extra1);
-                            printf("\n%d %s\n\n", carta.num, carta.nome);
+                            printf("\n\t-> Carta da rodada: %d %s\n\n", carta.num, carta.nome);
 
-                            pontoJ2 = pontoJ2 + menu(monte2, e1J2, e2J2, e3J2, e4J2, &extra2, carta);/*chama a função menu e incrementa no ponto se o espaço tiver 3 cartas*/
+                            pontoJ2 = pontoJ2 + menu(monte2, e1J2, e2J2, e3J2, e4J2, &extra2, carta);/*chama a funï¿½ï¿½o menu e incrementa no ponto se o espaï¿½o tiver 3 cartas*/
                             vez++;
                         }
                     }
                 }
             }
-            system("cls");
+            system("cls || clear");
         }
     }
     while(pontoJ1 != 5 && pontoJ2 != 5);  /*sai do while quando algum jogador vencer*/
 
 
-    if(vez %2==0)
+    if(pontoJ1 == 5)
     {
-        printf("~***********PARABENS, JOGADOR 1! VOCE VENCEU!***********~");
+        printf("\t****************************\n\t*          VENCEU!         *\n\t****************************\n\t*   Parabens, Jogador 1!   *\n\t****************************\n\t*    PLACAR: J1 %d x %d J2   *\n\t****************************\n", pontoJ1, pontoJ2);
         exit(1);
 
     }
-    else if(vez %2 != 0)
+    else if(pontoJ2 == 5)
     {
-        printf("~***********PARABENS, JOGADOR 2! VOCE VENCEU!***********~");
+        printf("\t****************************\n\t*          VENCEU!         *\n\t****************************\n\t*   Parabens, Jogador 2!   *\n\t****************************\n\t*    PLACAR: J1 %d x %d J2   *\n\t****************************\n", pontoJ1, pontoJ2);
         exit(1);
     }
 
@@ -245,7 +247,7 @@ int main()
     free(q);
     free(filaJ1);
     free(monte1);
-    free(&extra1);
+    free(extra1);
     free(e1J1);
     free(e2J1);
     free(e3J1);
@@ -254,7 +256,7 @@ int main()
     free(q);
     free(filaJ2);
     free(monte2);
-    free(&extra2);
+    free(extra2);
     free(e1J2);
     free(e2J2);
     free(e3J2);
